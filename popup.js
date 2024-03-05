f5615c0d44521135e91d0ef2e885df3c7dcf7b98
annanyavedala <126888755+annanyavedala@users.noreply.github.com>
2024-03-04 22:19:10 -0500

    Popup changes

--- popup.js
+++ popup.js
@@ -14,6 +14,31 @@
 
 
 //Start button functionality
+// This file defines the scripts behind each of the buttons and interactive elements
+// in the popup.html file for the popup window
+
+'use strict';
+
+function setAlarm(event) {
+  const minutes = parseFloat(event.target.value);
+  chrome.action.setBadgeText({ text: 'ON' });
+  chrome.alarms.create({ delayInMinutes: minutes });
+  chrome.storage.sync.set({ minutes: minutes });
+  window.close();
+}
+
+function clearAlarm() {
+  chrome.action.setBadgeText({ text: '' });
+  chrome.alarms.clearAll();
+  window.close();
+}
+
+// An Alarm delay of less than the minimum 1 minute will fire
+// in approximately 1 minute increments if released
+// document.getElementById('sampleMinute').addEventListener('click', setAlarm);
+// document.getElementById('min15').addEventListener('click', setAlarm);
+// document.getElementById('min30').addEventListener('click', setAlarm);
+// document.getElementById('cancelAlarm').addEventListener('click', clearAlarm);
 
 document.addEventListener('DOMContentLoaded', function() {
   const startBtn = document.getElementById('start');
